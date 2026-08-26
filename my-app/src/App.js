39cf74011a1d5aeaaf54903d8e2269bc31baf7a0
Nikhil-Prabhat <strangernikhilprabhat98@gmail.com>
2026-08-26 16:59:11 +0530

    RT-4 : Improving Dark Mode

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#1b1b67";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
   return (
     <>
-      <Navbar titleText="TextUtils" homeText="Home" />
-      
+      <Navbar
+        titleText="TextUtils"
+        homeText="Home"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+
       <div className="container">
-        <TextForm heading="Enter the text to analyze below"/>
+        <TextForm heading="Enter the text to analyze below" mode={mode}/>
       </div>
-      
     </>
   );
 }
