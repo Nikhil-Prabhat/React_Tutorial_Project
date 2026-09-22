73f83f350529cae0d37bcdefdae8467e65f9a27a
Nikhil-Prabhat <strangernikhilprabhat98@gmail.com>
2026-09-22 22:04:55 +0530

    RT-7 : Context Added

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -4,19 +4,21 @@ import Navbar from './component/Navbar';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './component/Home';
 import About from './component/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </NoteState>
     </>
-
   );
 }
 
--- inotebook/src/component/About.js
+++ inotebook/src/component/About.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
+import NoteContext from '../context/notes/NoteContext'
+import noteContext from '../context/notes/NoteContext'
 
 export default function About() {
+    const userContext = useContext(noteContext);
+    useEffect(() => {
+        userContext.update()
+    }, []);
+
     return (
         <div>
-            This is About.
+            This is About {userContext.state.name} who studies in {userContext.state.class}
         </div>
     )
 }
